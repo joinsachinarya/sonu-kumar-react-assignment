0679173d4a84016fa7f012bd5fc338c6f37f9a9d
Sachin Arya <sachinkaya333@gmail.com>
2024-02-17 04:33:55 +0530

    fix: formate

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Profile from './Profile.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <App/>,
+    path: "/",
+    element: <App />,
   },
   {
-    path:`profile/:id`,
-    element:<Profile />
+    path: `profile/:id`,
+    element: <Profile />
   }
 ])
 
