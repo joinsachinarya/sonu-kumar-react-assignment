262f1bca62a1dea15b25b7454c21a49541c62c2c
Sachin Arya <sachin.arya@preplaced.in>
2024-02-17 04:28:22 +0530

    feat: add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function App() {
+  let userPerPage = 2;
   const [users, setUsers] = useState(null);
-  
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -18,12 +20,18 @@ function App() {
   }, [])
 
 
+  const lastUserIndex = userPerPage * currentPage;
+  const firstUserIndex = lastUserIndex - userPerPage;
+  const currentShowingUsers = users && users.slice(firstUserIndex, lastUserIndex);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent:"center" }}>
+    <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
       <h1>All Users: 👇</h1>
       <ul style={{ listStyleType: "none", padding: 0, width: "100%", maxWidth: 900 }}>
-        {users && users.map((user) => (
-          <li key={user.id} style={{ marginBottom: 40, padding: 20, border: "1px solid #ccc", borderRadius:"12px", display: "flex", alignItems: "center" }}>
+        {currentShowingUsers && currentShowingUsers.map((user) => (
+          <li key={user.id} style={{ marginBottom: 40, padding: 20, border: "1px solid #ccc", borderRadius: "12px", display: "flex", alignItems: "center" }}>
             <img src={`https://i.pravatar.cc/150?u=${user.id}`} alt={user.name} style={{ borderRadius: "50%", marginRight: 10, flexShrink: 0, width: 50, height: 50 }} />
             <div>
               <h3 style={{ margin: 0 }}>Name: {user.name}</h3>
@@ -33,6 +41,17 @@ function App() {
           </li>
         ))}
       </ul>
+      <div>
+        {users &&
+          <ul style={{ display: "flex", justifyContent: "center", listStyleType: "none", padding: 0 }}>
+            {Array.from({ length: Math.ceil(users.length / userPerPage) }).map((item, index) => (
+              <li key={index} style={{ margin: 5 }}>
+                <button onClick={() => paginate(index + 1)}>{index + 1}</button>
+              </li>
+            ))}
+          </ul>
+        }
+      </div>
     </div>
   )
 }
